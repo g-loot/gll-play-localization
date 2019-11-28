671b5180c4c7366987e78eac0691a7b4ced080b0
Omar ElGaml <omar@gloot.com>
2019-11-28 15:23:32 +0100

    Automated translation ids update

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { convertPropsToFlatJson } from 'propson';
 // This is imported using Fusebox's raw plugin
 import enMessages from './messages/en-US/strings.properties';
 import viMessages from './messages/vi/strings.properties';
-import jaMessages from './messages/ja-JP/strings.properties';
-import koMessages from './messages/ko-KR/strings.properties';
-import trMessages from './messages/tr-TR/strings.properties';
-import thMessages from './messages/th-TH/strings.properties';
+import jaMessages from './messages/ja/strings.properties';
+import koMessages from './messages/ko/strings.properties';
+import trMessages from './messages/tr/strings.properties';
+import thMessages from './messages/th/strings.properties';
 import ruMessages from './messages/ru-RU/strings.properties';
 
 function returnMessagesFromPropertiesFile(rawPropertiesString) {
@@ -33,28 +33,28 @@ export default [
   },
   {
     name: 'Japanese',
-    locale: 'ja-JP',
+    locale: 'ja',
     contentfulLocale: 'ja-JP',
 
     messages: returnMessagesFromPropertiesFile(jaMessages),
   },
   {
     name: 'Korean',
-    locale: 'ko-KR',
+    locale: 'ko',
     contentfulLocale: 'ko-KR',
 
     messages: returnMessagesFromPropertiesFile(koMessages),
   },
   {
     name: 'Turkish',
-    locale: 'tr-TR',
+    locale: 'tr',
     contentfulLocale: 'tr-TR',
 
     messages: returnMessagesFromPropertiesFile(trMessages),
   },
   {
     name: 'Thailand',
-    locale: 'th-TH',
+    locale: 'th',
     contentfulLocale: 'th-TH',
 
     messages: returnMessagesFromPropertiesFile(thMessages),
@@ -62,7 +62,7 @@ export default [
   {
     name: 'Russian',
     locale: 'ru-RU',
-    contentfulLocale: 'ru-RU',
+    contentfulLocale: 'ru',
 
     messages: returnMessagesFromPropertiesFile(ruMessages),
   },
